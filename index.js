2650403fc596ac4f3d96ba7cc8e58a88703bb610
Samarthya21 <samarthya777@gmail.com>
2024-08-15 00:43:41 +0530

    Updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const { Server } = require("socket.io");
 const server = http.createServer(app);
 
-//all the streams is handled by socket.io
+// all the streams are handled by socket.io
 const io = new Server(server);
 
 io.on('connection', (socket) => { 
@@ -13,15 +13,16 @@ io.on('connection', (socket) => {
         console.log('A new user message', message);
         io.emit('message', message);    
     });
-    
-    
 });
-app.use(express.static(path.resolve('./public')));
 
-//http request handles by express 
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Handle HTTP requests
 app.get('/', (req, res) => { 
-    return res.sendFile('/public/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 server.listen(3000, () => {
     console.log("Server started at port 3000");
 });
